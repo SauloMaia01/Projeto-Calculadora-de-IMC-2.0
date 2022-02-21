44f71f467f06f011200654090f5b46cf3ca90c70
Saulo Rodrigues Maia <saulormaia@hotmail.com>
2022-02-21 16:00:27 -0300

    Termino do projeto

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ function go(currentStep,nextStep){
     if(currentStep == 1){
         dNone = firstDiv
     }else if(currentStep == 2){
-        dNome = secondDiv
+        dNone = secondDiv
     }else{
         dNone = finalDiv
     }
+    dNone.style.display = 'none'
 
     if(nextStep == 1){
         dBlock = firstDiv
@@ -21,37 +22,57 @@ function go(currentStep,nextStep){
     }else{
         dBlock = finalDiv
     }
-
     dBlock.style.display = 'block'
 } 
 
 function validate(){
-     
-    const peso   = document.getElementById('peso')
+
+    const peso = document.getElementById('peso')
     const altura = document.getElementById('altura')
-    peso.style.border   = 'none'
-    altura.style.border = 'none'
+   // peso.style.border = 'none'
+   // altura.style.border = 'none'
 
+    if(!peso.value || !altura.value){
 
-    if(!peso.value || !altura.value)
-    {
+        if(!peso.value && !altura.value){
 
-        if(!peso.value && !altura.value)
-        {
             peso.style.border = '1px solid red'
             altura.style.border = '1px solid red'
-        }
-        else if(!peso.value)
-        {
+
+        }else if(!peso.value){
             peso.style.border = '1px solid red'
-        }
-        else
-        {
+        }else{            
             altura.style.border = '1px solid red'
+        }  
+    }else{
+        let imc = peso.value / (altura.value * altura.value)
+        const result = document.getElementById('resultado')
+
+        if(imc < 18.5){
+            console.log('Magreza | Obesidade: 0')
+            result.style.color = 'yellow'
+            result.innerHTML = 'Magreza | Obesidade: 0'
+
+        }else if(imc >= 18.5 && imc < 25){
+            console.log('Normal | Obesidade: 0')
+            result.style.color = 'yellow'
+            result.innerHTML = 'Normal | Obesidade: 0'
+
+        }else if(imc >= 25 && imc < 30){            
+            console.log('Sobrepeso | Obesidade: I')
+            result.style.color = 'green'
+            result.innerHTML = 'Sobrepeso | Obesidade: I'
+
+        }else if(imc >= 30 && imc < 40){
+        console.log('Obsidade | Obesidade: II')
+        result.style.color = 'red'
+        result.innerHTML = 'Obsidade | Obesidade: II'
+
+        }else{
+            console.log('Obsidade Grave | Obesidade: III')
+            result.style.color = 'black'
+            result.innerHTML = 'Obsidade Grave | Obesidade: III'
         }
-    }
-    else
-    {
-        console.log('Tudo OK!! Pode calcular')
+        go(2,3)
     }
 }
